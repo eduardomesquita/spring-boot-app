bd42cb2912d53120e500a9e65f14fe37a0db3f42
Eduardo Mesquita <eduardomesquita09@gmail.com>
2016-10-23 18:16:35 -0200

    Alterando adicionando hibernate Spartial

--- codigo-fonte/src/main/java/br/com/test/repositorio/ClienteRepository.java
+++ codigo-fonte/src/main/java/br/com/test/repositorio/ClienteRepository.java
@@ -2,11 +2,9 @@ package br.com.test.repositorio;
 
 import br.com.test.entidades.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.repository.history.RevisionRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface ClienteRepository
-            extends RevisionRepository<Cliente, Long, Integer>,  JpaRepository<Cliente, Long> {
+public interface ClienteRepository extends JpaRepository<Cliente, Long> {
 
 }
